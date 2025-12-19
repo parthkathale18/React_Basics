3d971bb1607e2f843b8af6c306ac9eb10f56195e
parthkathale18 <your-email@example.com>
2025-12-19 19:46:34 +0530

    Added 07-localstorage folder

---
+++ 06-formhandling/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='enter your name' />
+        <button>submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
